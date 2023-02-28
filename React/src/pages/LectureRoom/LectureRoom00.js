e1b59f4a2af6de17804b54bf65e066c0534375ad
서혜진 <mz_shjin@woowafriends.com>
2023-02-28 15:11:18 +0900

    lectureroom layer guide

--- React/src/pages/LectureRoom/LectureRoom00.js
+++ React/src/pages/LectureRoom/LectureRoom00.js
@@ -1,17 +1,16 @@
 const LectureRoom00 = ({setPopName }) => {
     return (
         <>
-            <div className="lecture-room CtPop">
+            <div className="layer" />
+            <div className="lecture-room layer-right-center">
                 <button type="button" className="subclose close16" onClick={(e) => {setPopName();}} />
                 <div className="popTit">###title</div>
-                <div className="con-wrap">
-                    <div className="con-inner-col">
-                        <div class="modal-body">
-                            ####content
-                        </div>
-                        <div className="center mb10">
-                            <button type="button" className="sec-btn-L tit-20Gy3">확인</button>
-                        </div>
+                <div className="layer-wrap">
+                    <div className="layer-body">
+                        ####content
+                    </div>
+                    <div className="layer-footer center mb10">
+                        <button type="button" className="sec-btn-L tit-20Gy3">확인</button>
                     </div>
                 </div>
             </div>
